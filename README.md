795b46e44e046bd98079f20bf72db41b04ab18f9
Tom <dotato@Toms-MBP.TRUMP>
2019-03-07 06:19:53 -0500

    modified rebuild script

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 # Kali docker build with some basic packages
 # Open the 'base' directory and build the docker image from there
 # Scripts within the 'scripts' dir make for easy rebuilding & running
+# Run the 'KaliBNR.sh' rebuild script to build the image & start it
 # Rebuild script increments the tag by one from the current, then rebuilds and runs the image
--- scripts/KaliBNR.sh
+++ scripts/KaliBNR.sh
@@ -7,7 +7,9 @@ tags=( $(docker images --format '{{.Tag}}' tdalonzo/kali) )
 latestnum=$(echo ${tags[0]} | tr -dc '0-9')
 let "latestnum++"
 
+parentdir="$(dirname "$PWD")"
+
 echo "Rebuilding Kali Image"
-docker build -t tdalonzo/kali:base$latestnum -f /Users/dotato/Desktop/Docker/Kali/base/Dockerfile.Dockerfile .
+docker build -t tdalonzo/kali:base$latestnum -f $parentdir/base/Dockerfile.Dockerfile .
 echo "Starting Latest Kali Image"
 docker run --net kali_network --hostname kali4 --name kali -it tdalonzo/kali:base$latestnum
