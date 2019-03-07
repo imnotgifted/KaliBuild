11b7107e79a357d27b98dca866a1862ef82a11ed
Tom <dotato@Toms-MBP.TRUMP>
2019-03-06 20:18:54 -0500

    purging old files

--- Kali/Kalistart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# @dotato
-
-# Obtain latest tag for Kali image
-tags=( $(docker images --format '{{.Tag}}' kali/kali) )
-latestnum=$(echo ${tags[0]} | tr -dc '0-9')
-
-echo "Starting Latest Kali Image"
-docker run --net kali_network --hostname kali4 --name kali -it kali/kali:base$latestnum
